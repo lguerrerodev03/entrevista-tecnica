edb9192691fdf6728bb9d8c8936029abb7760767
Luis Guerrero <lguerrero03developer@gmail.com>
2025-04-09 17:33:09 -0500

    resolucion del problema 1

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let arreglo = [1, 2, 3, 4, 5];
+let n = 3;
+
+console.log(getCombinations(arreglo, n));
+
+function getCombinations(arr, k) {
+    let combinaciones = [];
+
+    function combinar(inicio, combinacion) {
+
+        // si la combinacion tiene el tamaño requerido en el parámetro se agrega
+        if (combinacion.length === k) {
+            combinaciones.push([...combinacion]);
+            return;
+        }
+
+        for (let i = inicio; i < arr.length; i++) {
+            combinacion.push(arr[i]);
+            // aqui se aplica recursividad
+            combinar(i+1, combinacion);
+            combinacion.pop();
+        }
+    }
+
+    combinar(0, []);
+
+    return combinaciones;
+}
+
+
